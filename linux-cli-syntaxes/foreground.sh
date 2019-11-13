241344cecd40a65f123e00f6e5b1b7cffeff3a64
KalluruS <>
2019-11-13 17:10:34 +0530

    --- Auto-Git Commit ---

--- linux-cli-syntaxes/foreground.sh
+++ linux-cli-syntaxes/foreground.sh
@@ -1,5 +1,5 @@
 # General Preparation
 systemctl restart iptables
-yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/noarch/podman-manpages-1.5.1-3.el7.noarch.rpm https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/x86_64/podman-1.5.1-3.el7.x86_64.rpm https://cbs.centos.org/kojifiles/packages/slirp4netns/0.3.0/2.git4992082.el7/x86_64/slirp4netns-0.3.0-2.git4992082.el7.x86_64.rpm https://cbs.centos.org/kojifiles/packages/libseccomp/2.4.1/0.el7/x86_64/libseccomp-2.4.1-0.el7.x86_64.rpm
+#yum install -y podman buildah skopeo skopeo-containers tree httpd-tools strace https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/noarch/podman-manpages-1.5.1-3.el7.noarch.rpm https://cbs.centos.org/kojifiles/packages/podman/1.5.1/3.el7/x86_64/podman-1.5.1-3.el7.x86_64.rpm https://cbs.centos.org/kojifiles/packages/slirp4netns/0.3.0/2.git4992082.el7/x86_64/slirp4netns-0.3.0-2.git4992082.el7.x86_64.rpm https://cbs.centos.org/kojifiles/packages/libseccomp/2.4.1/0.el7/x86_64/libseccomp-2.4.1-0.el7.x86_64.rpm
 podman pull registry.access.redhat.com/ubi7/ubi
 systemctl restart iptables
