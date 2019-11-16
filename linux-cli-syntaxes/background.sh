d01749fd7b28adda76a321d6302e40b52ce821f3
KalluruS <>
2019-11-16 13:19:27 +0530

    --- Auto-Git Commit ---

--- linux-cli-syntaxes/background.sh
+++ linux-cli-syntaxes/background.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 yum install httpd -y 
-systemctl start httpd
+
 curl -s https://raw.githubusercontent.com/devopstrainings/linux-basics-katakoda/master/linux-cli-syntaxes/assets/index.html >/var/www/html/index.html
+systemctl start httpd
