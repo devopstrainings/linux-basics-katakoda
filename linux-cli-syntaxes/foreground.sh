1a006b29dea5cdb71feaddd1cf40e789616f1574
KalluruS <>
2019-11-17 17:06:52 +0530

    --- Auto-Git Commit ---

--- linux-cli-syntaxes/foreground.sh
+++ linux-cli-syntaxes/foreground.sh
@@ -3,4 +3,4 @@ systemctl restart iptables
 yum install -y podman &>/dev/null 
 podman pull registry.access.redhat.com/ubi7/ubi &>/dev/null 
 clear 
-echo -e "\e["
+echo -e "\e[32m YOUR TERMINAL IS READY FOR USE"
