67bef29cb9c3c5c181cfe0f0d1a72022085db8ae
KalluruS <>
2019-11-17 17:27:14 +0530

    --- Auto-Git Commit ---

---
+++ linux-adminstration/background.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#yum install httpd -y 
+#curl -s https://raw.githubusercontent.com/devopstrainings/linux-basics-katakoda/master/linux-cli-syntaxes/assets/index.html >/var/www/html/index.html
+#systemctl start httpd
