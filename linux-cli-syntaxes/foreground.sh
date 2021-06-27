77b4294d9350d4e2efdd49de5e924ba88dab5448
KalluruS <>
2021-06-27 20:55:52 +0530

    ss

--- linux-cli-syntaxes/foreground.sh
+++ linux-cli-syntaxes/foreground.sh
@@ -1,4 +1,4 @@
 # General Preparation
 #curl -s https://raw.githubusercontent.com/devopstrainings/linux-basics-katakoda/master/linux-cli-syntaxes/assets/centos.sh | bash  
 
-
+export PS1='[ \u@centos-playground \w ] \\#  ' ; export HOME=/root; cd; clear
