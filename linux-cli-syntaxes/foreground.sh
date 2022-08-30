2617a578152a03af8e747d84ecb7073fe10aa39b
Raghu K <raghuk.vit@gmail.com>
2022-08-30 05:55:51 +0530

    ss

--- linux-cli-syntaxes/foreground.sh
+++ linux-cli-syntaxes/foreground.sh
@@ -2,4 +2,4 @@
 #curl -s https://raw.githubusercontent.com/devopstrainings/linux-basics-katakoda/master/linux-cli-syntaxes/assets/centos.sh | bash  
 
 #export PS1='[ \u@centos-playground \w ] \\#  ' ; export HOME=/root; cd; clear
-docker run -it centos:8 
+#docker run -it centos:8 
