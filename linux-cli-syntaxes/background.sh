8cda3613d448a4b815067b24c3b27fd2401f3bcd
Raghu K <raghuk.vit@gmail.com>
2022-08-30 06:01:02 +0530

    ss

--- linux-cli-syntaxes/background.sh
+++ linux-cli-syntaxes/background.sh
@@ -4,3 +4,4 @@
 #curl -s https://raw.githubusercontent.com/devopstrainings/linux-basics-katakoda/master/linux-cli-syntaxes/assets/index.html >/var/www/html/index.html
 #systemctl start httpd
 
+echo hello 
